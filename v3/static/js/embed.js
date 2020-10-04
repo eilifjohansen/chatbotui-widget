0dd246d75059712491e92ade4b1bf388007d5ed5
Eilif Johansen <eilif@eilifjohansen.no>
2020-10-04 22:47:42 +0200

    Improved font size

--- v3/static/js/embed.js
+++ v3/static/js/embed.js
@@ -4,7 +4,7 @@ if (!botcolor) {
 
 // SVG Icons
 sendbot =
-  '<svg id="sendButtonIcon" style="margin-top: -2.5px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="undefined ns-fill-0" fill="' +
+  '<svg id="sendButtonIcon" style="margin-top: -2px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="undefined ns-fill-0" fill="' +
   botcolor +
   '" width="25" height="25"> <path d="M3.4 20.4l17.45-7.48a1 1 0 0 0 0-1.84L3.4 3.6a.993.993 0 0 0-1.39.91L2 9.12c0 .5.37.93.87.99L17 12L2.87 13.88c-.5.07-.87.5-.87 1l.01 4.61c0 .71.73 1.2 1.39.91z"/></svg>';
 closebot =
