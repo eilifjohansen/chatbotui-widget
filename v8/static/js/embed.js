da448a0aa59e55e5803813390a8e8d05cd6a79b0
Eilif Johansen <eilif@eilifjohansen.no>
2020-11-18 22:50:16 +0100

    Altered chat speed

--- v8/static/js/embed.js
+++ v8/static/js/embed.js
@@ -630,10 +630,11 @@ setTimeout(() => {
           // Time to wait before showing the next message.
           // We'll make it a random number between 200 and 600 to make it dynamic.
           /* console.log(ms); */
-          /*
-          const ms = Math.random() * (600 - 200) + 200;
-          await sleep(ms);
-          */
+
+          if (!response[i].hasOwnProperty("buttons")) {
+            const ms = Math.random() * (600 - 200) + 200;
+            await sleep(ms);
+          }
         }
       }
 
@@ -643,7 +644,7 @@ setTimeout(() => {
           addSuggestion(response[i].buttons);
         }
       }
-    }, 400);
+    }, 500);
   }
 
   //====================================== Toggle chatbot =======================================
